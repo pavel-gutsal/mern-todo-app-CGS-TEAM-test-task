ecd3a554010b08f0231233830214a049b5c7e225
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-11-15 22:28:27 +0200

    commit

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './GlobalStyles/GlobalStyles';
 import { theme } from './Theme/Theme';
@@ -13,11 +13,11 @@ const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
